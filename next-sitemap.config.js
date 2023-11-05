d8cb2d420c467846834c95f305f34a637555a768
shivang-stack <shivang1691401@gmail.com>
2023-11-05 19:49:57 +0530

    sitemap add

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+    siteUrl: 'https://nextjs-seo-ecom-f1zo5y1oj-shivang-stack.vercel.app',
+    generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+    // Other available options..
+  }
+ 
+module.exports = config;
